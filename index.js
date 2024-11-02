c8631c3dc88da9332d58f848b27c5417f232d20b
Remzo00 <gusinacremzo00@gmail.com>
2024-11-02 09:29:44 +0100

    add CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { config as connectDB } from "./src/config/config.js";
 import authRouter from "./src/routes/authRoutes.js";
 
@@ -8,6 +9,8 @@ dotenv.config();
 const app = express();
 
 connectDB();
+
+app.use(cors({ origin: "http://localhost:5173" }));
 
 app.use(express.json());
 app.use(authRouter);
